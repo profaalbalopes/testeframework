0887430a1c97c5d6a717e8dacaa7fba054f3807e
Alba Lopes <albasandyra@gmail.com>
2021-02-10 17:09:05 -0300

    Inicializando repositório

---
+++ application/views/formas_de_contato.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formas de contato</title>
+    </head>
+    <body>
+        <h1>Entre em contato através do seguintes meios</h1>
+        <ul>
+            <li> E-mail </li>
+            <li> Chat </li>
+            <li> Telefone </li>
+            <li> Lojas físicas </li>
+        </ul>
+    </body>
+</html>
+
---
+++ application/views/principal.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Página Principal</title>
+    </head>
+    <body>
+        <p>Essa é a nova página principal do meu site</p>
+    </body>
+</html>
